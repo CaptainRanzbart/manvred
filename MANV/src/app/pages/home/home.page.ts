bbc93356d84a11983ff29f0d8062d45c57a2aeb1
Adrian Tischler <a.tischler18596@hs-ansbach.de>
2024-06-13 10:12:20 +0200

    added a modal to home page

--- MANV/src/app/pages/home/home.page.ts
+++ MANV/src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild(IonModal, { static: true }) modal!: IonModal;
 
-  constructor() { }
+  message = 'Untersuchung:';
+  name: string = '';
+  names: string[] = [];
 
-  ngOnInit() {
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
   }
 
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+     
+      this.names.push(ev.detail.data!);
+      this.message = `Untersuchungen: `;
+      this.name='';
+      this.saveNames();
+    }
+  }
+  removeName(index: number) {
+    this.names.splice(index, 1);
+    this.saveNames();
+  }
+
+  constructor() {}
+
+  ngOnInit() { 
+    this.loadNames();
+  }
+  saveNames() {
+    localStorage.setItem('names', JSON.stringify(this.names));
+  }
+  loadNames() {
+    const storedNames = localStorage.getItem('names');
+    if (storedNames) {
+      this.names = JSON.parse(storedNames);
+    }
+  }
+ 
+
+ 
 }
